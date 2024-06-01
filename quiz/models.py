964a7ed8c257700e223e376e0460857244c3438a
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-06-01 20:05:21 +0330

    last change added.

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name =   models.CharField(max_length=255)
+    def __str__(self):
+        return self.name 
+class Question (models.Model):
+    id = models.AutoField(primary_key=True)
+    content = models.TextField()
+    category = models.CharField(max_length=100, null=True, default=None)
+    image = models.ImageField(upload_to='quiz/',default='quiz/default.jpg')
+    audio_file = models.FileField(upload_to='audio/')
+    score = models.IntegerField(default = 0)
+    created_date = models.DateTimeField(auto_now_add = True)
+    updated_date = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.title
--- quiz/views.py
+++ quiz/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 def test_view(request):
     context = {}
-    return render(request, 'base.html',context)
+    return render(request,'base.html',context)
