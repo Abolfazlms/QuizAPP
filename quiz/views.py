fc5c67a0136192a42dcbcdb8e59556877ed6c6c3
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-06-21 22:22:47 +0330

    project cleaned.
    project structure are cleaned and ready to deploy.

--- quiz/views.py
+++ quiz/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
 from quiz.models import Question, Choice, UserTestResult, UserAnswer, Test
 from django.contrib.auth.decorators import login_required
-from django.template.loader import get_template
-from django.utils.html import escape
-
-from xhtml2pdf import pisa
-from django.conf import settings
-import os
 
 import sweetify
 
@@ -18,27 +11,6 @@ def test_view(request):
     context = {'quiz':quiz}
     return render(request,'test.html',context)
 
-# def quiz_single(request,qid):
-    
-#     quests = Question.objects.all()
-#     quiz = get_object_or_404(quests,pk=qid)
-#     nextquiz = quests.filter(id__gt = qid).first() 
-#     if qid == 1 :  
-#         sweetify.info(request, type='success',title='راهنما',text='برای شنیدن فایل صوتی راهنمای سوالات، بر روی علامت پخش موسیقی در بالا سمت راست تصویر کلیک کنید.', timer=4000)
-
-#         if request.method == 'POST':
-#             if request.POST.get('choice1'):
-#                 quiz.choice = 1
-#                 quiz.save()
-#             if request.POST.get('choice2'):
-#                 quiz.choice = 2
-#                 quiz.save()
-#             if request.POST.get('choice3'):
-#                 quiz.choice = 3
-#                 quiz.save()
-
-#     content = {'quiz':quiz,'next':nextquiz,'all_quiz':quests.count(),'number':list(quests).index(quiz)+1}    
-#     return render(request,'quiz/quiz-single.html',content)
 
 @login_required
 def quiz_single(request, qid):
