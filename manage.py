5ab0a1ef19916763e186498e0145af6378c4f6ad
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-06-24 11:25:53 +0330

    project optimization are ended.
    project are optimized.

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quizeApplication.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quizeApplication.setting.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
---
+++ quizeApplication/setting/prod.py
@@ -0,0 +1,28 @@
+from quizeApplication.settings import *
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-(0%m(^nwm7zk=mlv&yckopkpq1-!pe*qh_&1fi*ix8m)xrjjor'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['heyweb.ir','www.heyweb.ir']
+# INSTALLED_APPS = [
+# ]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'statics',
+]
+
+CSRF_COOKIE_SECURE = True
